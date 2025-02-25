ad97a43f4a532a36fa40916b963f816afd57a5b6
Akash Kurdekar <akashkurdekar39@gmail.com>
2025-02-25 16:19:40 +0530

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,52 +8,35 @@ const Navbar = () => {
     <nav className="fixed top-2 left-1/2 transform -translate-x-1/2 w-[70%] bg-primary text-white rounded-lg shadow-md p-7  font-bold">
       <div className="flex items-center md:justify-center sm:justify-end ">
         <button
-          className="text-2xl text-white md:hidden"
+          className="text-4xl text-white md:hidden"
           onClick={() => setIsOpen(!isOpen)}
         >
           <FaBars />
         </button>
 
         <ul
-          className={`absolute md:static top-14 left-0 w-full md:w-auto bg-primary md:bg-transparent flex flex-col md:flex-row md:space-x-6 p-4 md:p-0 rounded-lg md:rounded-none transition-all duration-300 ease-in-out ${
-            isOpen ? "block" : "hidden md:flex"
+          className={`absolute md:static top-24 left-0 w-full md:w-auto bg-primary md:bg-transparent flex flex-col md:flex-row md:space-x-6 p-4 md:p-0 rounded-lg md:rounded-none transition-all duration-400 ease-in-out   ${
+            isOpen ? "block" : "hidden md:flex "
           }`}
         >
-          <li className="p-2">
-            <a href="#" className="text-lg hover:text-bgc font-signature">
-              Home
-            </a>
-          </li>
-          <li className="p-2">
-            <a href="#" className="text-lg hover:text-bgc font-signature">
-              About
-            </a>
-          </li>
-          <li className="p-2">
-            <a href="#" className="text-lg hover:text-bgc font-signature">
-              Work
-            </a>
-          </li>
-          <li className="p-2">
-            <a href="#" className="text-lg hover:text-bgc font-signature">
-              Skills
-            </a>
-          </li>
-          <li className="p-2">
-            <a href="#" className="text-lg hover:text-bgc font-signature">
-              Education
-            </a>
-          </li>
-          <li className="p-2">
-            <a href="#" className="text-lg hover:text-bgc font-signature">
-              Projects
-            </a>
-          </li>
-          <li className="p-2">
-            <a href="#" className="text-lg hover:text-bgc font-signature">
-              Contact
-            </a>
-          </li>
+          {[
+            "Home",
+            "About",
+            "Work",
+            "Skills",
+            "Education",
+            "Projects",
+            "Contact",
+          ].map((item) => (
+            <li key={item} className="p-2">
+              <a
+                href={`#${item.toLowerCase()}`}
+                className="flex justify-end font-mono text-lg hover:text-bgc"
+              >
+                {item}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
     </nav>
