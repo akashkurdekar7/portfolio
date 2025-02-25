0c88b14c8a98de81e1b3eea1960127d4d6884dab
Akash Kurdekar <akashkurdekar39@gmail.com>
2025-02-25 23:06:44 +0530

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,26 +2,30 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <section className="flex flex-col items-center justify-center h-auto text-center pb-9 gap-44 bg-bgc">
+    <section className="flex flex-col items-center justify-center h-auto gap-20 text-center pb-9 bg-bgc md:gap-44">
       <div className="w-full">
-        <h1 className="text-4xl font-bold font-signature text-primary">
+        <h1 className="text-4xl font-bold font-signature text-primary sm:text-2xl md:text-3xl">
           Akash
         </h1>
         <hr className="w-full my-4 border-t-2 border-black" />
       </div>
 
-      <div className="w-full">
-        <h1 className="text-8xl font-signature text-primary">LET'S WORK</h1>
-        <h1 className="font-serif text-6xl text-black uppercase">TOGETHER</h1>
+      <div className="flex flex-col items-center justify-center w-full text-center md:flex-row">
+        <h1 className="text-6xl font-signature text-primary sm:text-3xl md:text-8xl">
+          LET'S WORK
+        </h1>
+        <h1 className="font-serif text-4xl text-black uppercase sm:text-2xl md:text-6xl">
+          TOGETHER
+        </h1>
       </div>
 
       <div className="w-full">
         <hr className="w-full my-4 border-t-2 border-black" />
-        <div className="flex justify-between gap-2 text-lg font-medium">
+        <div className="flex flex-col justify-between gap-2 text-lg font-medium md:flex-row sm:text-sm md:text-lg">
           <h4 className="text-black">+91-9916390580</h4>
           <a
             href="mailto:akashkurdekar39@gmail.com"
-            className=" hover:underline"
+            className="hover:underline"
           >
             akashkurdekar39@gmail.com
           </a>
